8778746ff6b4dd887c23028fbd4735cd8f60b4f5
dansal0807 <danielsaldanha0@gmail.com>
2021-10-17 18:56:11 -0300

    Download de todas as imagens e criação de pasta de repositório.

--- image-shower.py
+++ image-shower.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import requests
 from tkinter import filedialog
 from webscraping import WebScraping
+import os
 
 #instanciamento do tkinter:
 ws = tk.Tk()
@@ -15,63 +16,55 @@ canvas.grid(columnspan=8, rowspan=8)
 instructions = tk.Label(ws, bg="#AFEEEE", text="Digite a URL:", font=('Helvatical bold',11))
 instructions.grid(columnspan=8, column=0, row=1)
 
-instructions2 = tk.Label(ws, bg="#AFEEEE", text="Digite a URL da imagem:", font=('Helvatical bold',11))
-instructions2.grid(columnspan=8, column=0, row=4)
-
 #variáveis:
 urlvar = tk.StringVar()
-imgvar = tk.StringVar()
 
 
 #Input do usuário:
 urlentry=tk.Entry(ws, textvariable=urlvar, width=50, font=("default", 11))
 urlentry.grid(columnspan=8, column=0, row=2)
 
-imgvar=tk.Entry(ws, textvariable=imgvar, width=50, font=("default", 11))
-imgvar.grid(columnspan=8, column=0, row=5)
-
-def popupmsg(msg):
-    popup = tk.Tk()
-    popup.wm_title("Sucesso")
-    label = tk.Label(popup, text=msg)
-    label.pack(side="top", fill="x", pady=10)
-    B1 = tk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack(pady=5)
-    popup.mainloop()
-
 #função de download:
-def setoptions():
+def downloadimages():
     options = []
     url_link=urlvar.get()
-    if '.jpg' or '.png' not in url_link:
-        options = WebScraping.soup(url_link)
-    else:
-        options = url_link
-    message = "esses são os links de imagens:\n\n"
-    for option in options:
-        message += option + '\n'
+
+    options = WebScraping.soup(url_link)
+
+    website_name = url_link.split('//')
+    website_name = website_name[1]
+
+    c = 0
+    for imgs in options:
+        if '.svg' not in imgs:
+            c += 1
+
+    message = f"Existem {c} imagens no link indicado."
     imglinks = tk.Label(ws, bg="#AFEEEE", text= f'{message}', font=('Helvatical bold',11))
     imglinks.grid(columnspan=8, column=0, row=3)
+    
+    current_dic = os.getcwd()
+    if not os.path.exists(website_name):
+        os.mkdir(website_name)
 
-optionButton = tk.Button(text='Links para imagem', command=setoptions, bg='green', fg='white', font=('Arial', 12, 'bold'))
+    os.chdir(website_name)
+
+    i = 0
+    for imgs in options:
+        if '.svg' not in imgs:
+            i += 1
+        try:
+            url_img = requests.get(imgs)
+        except ValueError:
+            print("Não foi possível acessar esta url.")
+        f = open(f'img{i}.jpg', 'wb')
+        f.write(url_img.content)
+        f.close()
+    os.chdir(current_dic)
+    
+
+optionButton = tk.Button(text='Baixar imagens', command=downloadimages, bg='green', fg='white', font=('Arial', 12, 'bold'))
 canvas.create_window(150, 180, window=optionButton)
-optionButton.grid(columnspan=8, column=6, row=2)
-
-
-def image_downloader():
-    url_link=urlvar.get()
-    file_request = filedialog.asksaveasfilename(defaultextension='.jpg')
-    url_link = requests.get(url_link)
-    f = open(file_request, 'wb')
-    f.write(url_link.content)
-    f.close()
-    popupmsg("Seu download foi bem sucedido!")
-#Não há necessidade de usar a função save, pois se pretende não fazer uma lista de arquivos.
-
-
-#Butão utilizado pelo usuário:
-saveAsButton = tk.Button(text='Baixar imagem', command=image_downloader, bg='green', fg='white', font=('Arial', 12, 'bold'))
-canvas.create_window(150, 180, window=saveAsButton)
-saveAsButton.grid(columnspan=8, column=6, row=5)
+optionButton.grid(columnspan=8, column=0, row=4)
 
 ws.mainloop()
--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,7 @@
 
 import requests 
 from bs4 import BeautifulSoup 
+import os
 
 class WebScraping(): 
     def soup(html):
